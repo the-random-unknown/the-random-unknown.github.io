24f38f2d075f9bbe6f943b7a9ff257e91723a97b
ondrejlednicky <54577619+ondrejlednicky@users.noreply.github.com>
2023-12-10 19:59:52 +0100

    Update events.js

--- js/events.js
+++ js/events.js
@@ -64,23 +64,23 @@ kazoni_voda[0] = async function () {
 	await output.write("Při cestě galaxií jsi potkal zastaralou nákladní loď, která tě žádá o pomoc. Poznáváš výsostné znaky Ocampů");
 
 	output.createButton("Rozhodneš se pomoci", kazoni_voda[2]);
-	output.createButton("Využiješ své převahy a rozhodneš se loď zničit" + battle, kazoni_voda[3]);
+	output.createButton("Rozhodneš se loď zničit" + battle, kazoni_voda[3]);
 	output.createButton("Na prosbu nereaguješ a letíš jinam" + jump, generateRandomEvent);
 };
 
 kazoni_voda[2] = async function () {
 	await output.write("Loď skutečně patří Ocampům, kteří žijí na své planetě Ocampa pod nadvládou Kazonů, v otroctví. Na planetě je kritický nedostatek vody, proto má nákladní loď za úkol najít asteroid se zásobou ledu, která po vytěžení přiveze na Ocampu. Kapitán a zároveň jediný člen posádky lodi ti přislíbí velkou odměnu v podobě paliva a surovin, kterých mají na planetě přebytek, pokud jim pomůžeš nalézt a dovézt na planetu vodu.");
 
-	output.createButton("Použiješ své dálkové senzory k nalezení vhodného asteroidu s vodou", kazoni_voda[4]);
-	output.createButton("Nezajímá tě to, rozloučíš se a odlétáš" + jump, generateRandomEvent);
+	output.createButton("Pomůžeš nalézt vhodný asteroid s vodou", kazoni_voda[4]);
+	output.createButton("Nezajímá tě to, odlétáš" + jump, generateRandomEvent);
 	output.createButton("Rozhodneš se nákladní loď obsadit a vyrabovat" + battle, kazoni_voda[5]);
 };
 
 kazoni_voda[3] = async function () {
 	new Battle(6, 5, async function () {
-		await output.write("V troskách lodi se žádný materiál nenachází.")
+		await output.write("V troskách lodi se žádný materiál nenachází. Boj tě stál jednu jednotku paliva a poškodil sis karmu s Ocampy")
 
-		output.createButton("Skočit dál (-1 k palivu za bojové manévry, -30% karma pro Ocamp)" + jump, function () {
+		output.createButton("Skočit dál" + jump, function () {
 			inventory.addFuel(-2);
 			factions.addRelation(2, -30);
 			generateRandomEvent();
@@ -89,19 +89,20 @@ kazoni_voda[3] = async function () {
 }
 
 kazoni_voda[4] = async function () {
-	await output.write("Podařilo se ti najít vhodný asteroid, vytěžíte led a vracíte se na Ocamp. Na oběžné dráze hlídkuje loď Kazonů, vypadá trochu slabší, než ty.");
+	await output.write("Podařilo se ti najít vhodný asteroid, vytěžíte led a vracíte se na Ocamp. Na oběžné dráze hlídkuje loď Kazonů, vypadá trochu slabší, než ty. Můžes s Kazony bojovat, misi ukončit nebo se s Kazony spojit a předat jim loď Ocampů.");
 
 	output.createButton("Zaútočíš na Kazony" + battle, kazoni_voda[6]);
-	output.createButton("Za boj ti zisk nestojí. (-2 k palivu, -10 karma pro Ocamp, +10% karma pro Kazony)", function () {
+	output.createButton("Letíš jinam", function () {
 		inventory.addFuel(-2);
 		factions.addRelation(3, -10);
 		factions.addRelation(2, 10);
 		generateRandomEvent();
 	})
 
-	output.createButton("Spojíš se s Kazony a loď Ocampů jim předáš (-1 k palivu, +30% karma pro Kazony)", function () {	
+	output.createButton("Předáš loď Ocampů Kazonům", function () {	
 		inventory.addFuel(-1);
-		factions.addRelation(2, 30);
+		factions.addRelation(2, -30);
+		factions.addRelation(1, 20);
 		generateRandomEvent();
 	})
 }
@@ -109,7 +110,7 @@ kazoni_voda[4] = async function () {
 kazoni_voda[5] = async function () {
 	new Battle(2, 2, async function () {
 		await output.write("Kapitán napadení nečekal, podařilo se ti ho přemoci. Bohužel na lodi není nic, co bys mohl použít.");
-		output.createButton("Pokračovat (Karma pro Ocamp -30%)", function () {
+		output.createButton("Pokračovat", function () {
 			factions.addRelation(3, -30);
 			generateRandomEvent();
 		});
@@ -120,12 +121,12 @@ kazoni_voda[6] = async function () {
 	new Battle(10, 5, async function () {
 		await output.write("Máš volnou cestu na planetu.");
 		if (inventory.getScrap() > 2) {
-			output.createButton("-2 palivo za bojové manévry, -2 materiál na opravy", kazoni_voda[7]);
+			output.createButton("Letíš dolů", kazoni_voda[7]);
 			inventory.addFuel(-2);
 			inventory.addScrap(-2);
 		}
 		else {
-			output.createButton("-2 palivo za bojové manévry, -20% zdravi z nedostatku materiálu na opravy", kazoni_voda[7]);
+			output.createButton("Letíš dolů", kazoni_voda[7]);
 			inventory.addFuel(-2);
 			inventory.addHealth(-20);
 		}
@@ -149,6 +150,8 @@ kazoni_voda[7] = async function () {
 		inventory.addScrap(5);
 		generateRandomEvent();
 	})
+	inventory.addItem('kazoni_voda_completed');
+	
 }
 
 //============================ Kazoni - 02 - Odplata ============================
@@ -177,12 +180,12 @@ let kazoni_odplata = [];
 events.push(kazoni_odplata);
 
 kazoni_odplata[0] = async function () {
-	if(!isCompleted(kazoni_voda)) generateRandomEvent();
+	if(!inventory.getItem('kazoni_voda_completed')) generateRandomEvent();
 	destination.setName("Planeta " + getRadnomCode());
 	destination.setFaction(1);
 	destination.setStatus(0);
 
-	await output.write("Kazoni ti nezapomněli pomoc, kterou jsi poskytl Ocampům. Poslali proti tobě 2 válečné lodi stejného typu, jako ta, kterou jsi u Ocamp již potkal");
+	await output.write("Kazoni ti nezapomněli pomoc, kterou jsi poskytl Ocampům. Poslali proti tobě válečnou loď, kterou jsi u Ocamp již potkal");
 
 	output.createButton("Pokusíš se uniknout do blízkého oblaku ionizovaného plynu", function () {
 			inventory.addScrap(-1);
